d7cfacd31f3a43af54d235af325451522ca278e7
IamMarkReyRonolo <ronolomarkrey@gmail.com>
2023-06-14 20:30:37 +0800

    added views

---
+++ src/views/LogInPage.vue
@@ -0,0 +1,119 @@
+<template>
+	<div class="logInPageCon">
+		<div class="headerPageCon">
+			<div class="logoCon">
+				<img src="../assets/cor-jesu-college-logo.jpg" alt="" />
+			</div>
+			<div class="titleCon">CJC - Multipurpose Cooperative</div>
+		</div>
+		<div class="logInFormContainer">
+			<div class="containerHeader">
+				<h1>Login</h1>
+				<v-select
+					:items="roles"
+					label="Login as"
+					filled
+					color="#78C66B"
+					dense
+				></v-select>
+			</div>
+
+			<div class="formFields">
+				<v-text-field
+					label="Email"
+					outlined
+					color="#78C66B"
+					:rules="usernameRules"
+				></v-text-field>
+				<v-text-field
+					label="Password"
+					outlined
+					color="#78C66B"
+					:rules="passwordRules"
+					:append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+					:type="show ? 'text' : 'password'"
+					@click:append="show = !show"
+				></v-text-field>
+				<v-btn color="success" dark large block>Log in</v-btn>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Home",
+		components: {},
+		data: () => ({
+			show: false,
+			roles: ["User", "Admin", "Credit Committee"],
+			usernameRules: [
+				(v) => !!v.trim() || "This field is required",
+				(v) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v) || "Email must be valid",
+			],
+			passwordRules: [(v) => !!v || "This field is required"],
+		}),
+	};
+</script>
+
+<style scoped>
+	.headerPageCon {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 40px 0px;
+		margin-top: 50px;
+	}
+	.logoCon,
+	.logoCon img {
+		width: 80px;
+	}
+
+	.titleCon {
+		padding: 10px;
+		font-size: 20px;
+		font-weight: 500;
+	}
+	.logInFormContainer {
+		position: absolute;
+		top: 55%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		width: 400px;
+		padding: 40px 48px;
+		border: 1px solid #dadce0;
+		border-radius: 8px;
+		background-color: white;
+		/* box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1); */
+	}
+
+	h1 {
+		margin-bottom: 20px;
+		text-align: center;
+		font-weight: 500;
+		font-size: 24px;
+	}
+
+	@media only screen and (max-width: 500px) {
+		.headerPageCon {
+			flex-direction: column;
+			margin: 20px;
+			padding-top: 40px;
+		}
+
+		.titleCon {
+			font-size: 16px;
+		}
+		.logInFormContainer {
+			position: relative;
+			border: none;
+			top: 0%;
+			left: 0%;
+			transform: translate(0%, 0%);
+			max-width: 360px;
+			padding: 0px 20px;
+			margin: auto;
+			margin-top: 0px;
+		}
+	}
+</style>
---
+++ src/views/MemberHome.vue
@@ -0,0 +1,200 @@
+<template>
+	<div class="memberHomeCon">
+		<div class="headerPageCon">
+			<div class="navlineCon">
+				<div class="logoCon">
+					<div class="imgCon">
+						<img src="../assets/cor-jesu-college-logo.jpg" alt="" />
+					</div>
+					<div class="logoTitleCon">CJC - Multipurpose Cooperative</div>
+				</div>
+				<div class="navCon">
+					<v-btn small text @click="clickedSettings = true">Settings</v-btn>
+					<v-btn small text @click="clickedLogOut = true">Log out</v-btn>
+				</div>
+				<div class="dropDown">
+					<DropDown
+						@clickedSettings="clickedSettings = true"
+						@clickedLogOut="clickedLogOut = true"
+					/>
+				</div>
+			</div>
+			<div class="userDetailsCon">
+				<div class="userProfileCon">
+					<div class="profilePicCon">
+						<img src="../assets/avatarprofilefemale.png" alt="" />
+					</div>
+					<div class="nameCon">JESSICA DOE</div>
+				</div>
+
+				<div class="shareCapitalCon">
+					<div class="sectionTitleCon">Share Capital</div>
+					<div class="amountFieldCon">
+						<div class="amount">₱ 50,000.00</div>
+						<v-btn text fab @click="clickedAccountLedger = true"
+							><v-icon>mdi-dots-horizontal</v-icon></v-btn
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="mainContentCon">
+			<LoanTable />
+		</div>
+		<LogOut
+			v-if="clickedLogOut"
+			:clickedLogOut="clickedLogOut"
+			@cancelLogOut="clickedLogOut = false"
+		/>
+
+		<Settings
+			v-if="clickedSettings"
+			:clickedSettings="clickedSettings"
+			@closeSettings="clickedSettings = false"
+		/>
+
+		<AccountLedger
+			v-if="clickedAccountLedger == true"
+			:clickedAccountLedger="clickedAccountLedger"
+			@closeLedger="clickedAccountLedger = false"
+		/>
+	</div>
+</template>
+
+<script>
+	import LoanTable from "../components/MemberPageComponents/LoanTable";
+	import DropDown from "../components/MemberPageComponents/DropDownMenu";
+	import Settings from "../components/MemberPageComponents/Settings";
+	import LogOut from "../components/MemberPageComponents/LogOut";
+	import AccountLedger from "../components/MemberPageComponents/AccountLedger";
+	export default {
+		components: { LoanTable, LogOut, DropDown, Settings, AccountLedger },
+		data: () => ({
+			clickedLogOut: false,
+			clickedSettings: false,
+			clickedAccountLedger: false,
+		}),
+	};
+</script>
+
+<style scoped>
+	.headerPageCon {
+		padding: 20px 50px;
+		border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+	}
+	.navlineCon {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.logoCon {
+		display: flex;
+		align-items: center;
+	}
+
+	.imgCon img {
+		width: 40px;
+	}
+
+	.logoTitleCon {
+		padding: 0px 10px;
+		font-size: 14px;
+	}
+
+	.userDetailsCon {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 20px 0px;
+	}
+
+	.userProfileCon {
+		display: flex;
+		align-items: center;
+	}
+
+	.profilePicCon img {
+		width: 90px;
+		border-radius: 100%;
+	}
+
+	.nameCon {
+		padding: 0px 20px;
+		font-weight: 500;
+		font-size: 18px;
+	}
+
+	.amountFieldCon {
+		border: 1px solid black;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0px 10px;
+		width: 225px;
+		border-radius: 5px;
+		margin: 5px 0px;
+	}
+
+	.sectionTitleCon {
+		font-weight: 500;
+	}
+
+	.mainContentCon {
+		padding: 20px 50px;
+		/* background-color: #f7f7f7; */
+	}
+
+	.dropDown {
+		display: none;
+	}
+
+	@media only screen and (max-width: 900px) {
+		.headerPageCon {
+			padding: 20px 20px;
+		}
+		.mainContentCon {
+			padding: 20px 10px;
+			/* background-color: #f7f7f7; */
+		}
+	}
+
+	@media only screen and (max-width: 600px) {
+		.headerPageCon {
+			padding: 20px 20px;
+		}
+
+		.mainContentCon {
+			padding: 20px 10px;
+			/* background-color: #f7f7f7; */
+		}
+		.navCon {
+			display: none;
+		}
+
+		.userDetailsCon {
+			display: block;
+		}
+
+		.profilePicCon img {
+			width: 100px;
+		}
+
+		.nameCon {
+			font-size: 20px;
+		}
+
+		.amountFieldCon {
+			width: 275px;
+			padding: 5px 10px;
+		}
+
+		.userProfileCon {
+			padding-bottom: 20px;
+		}
+
+		.dropDown {
+			display: block;
+		}
+	}
+</style>
